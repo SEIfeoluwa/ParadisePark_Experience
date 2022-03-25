b559c3a5b7d1aff8f2eaa314da168cfea90c49e9
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-03-25 11:26:13 -0400

    save

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -5,10 +5,10 @@ const Landing = () => {
     return (
        <div>
          <h1>Welcome to Paradise Park Feedback</h1>
-        <Link to='/read'><button>
+        <Link to='/review'><button>
             Write Review
         </button></Link>
-        <Link to='/review'><button>
+        <Link to='/read'><button>
             Read Reviews
         </button></Link>   
     </div> 
