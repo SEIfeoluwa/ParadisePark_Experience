600c047822e7cf38bb0abdd4c11a107cd02f2cb5
Ryan Hatamosa <pinoydol.colada@gmail.com>
2022-03-25 09:58:16 -0400

    cleaned code

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 const controllers = require('../controllers/reviews');
 const router = Router();
 
-router.get('/', (req, res) => res.send('This is user!'));
 router.get('/reviews', controllers.getAllReviews);
 
 router.post('/createReviews', controllers.createReview);
